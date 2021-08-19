462f8a62280df1e80d028a1c20d3d364988b8f07
Blas Gomez <jblas1989@gmail.com>
2021-08-19 19:18:13 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 // JS Goes here - ES6 supported
-
 import "./css/main.css";
 
 // Say hello
 /* eslint no-console: ["error", { allow: ["log"] }] */
 console.log("🦊 Hello! Edit me in src/index.js");
+
+var cloudinary = require('cloudinary-core');
+var tag = cloudinary.ImageTag.new("sample", {cloud_name: "demo", width: 300});
+document.body.appendChild(tag.toDOM());
+document.body.appendChild(
+    tag.transformation()
+        .angle(15)
+        .width(200)
+        .crop("scale")
+        .effect("grayscale")
+        .getParent().toDOM()
+);
